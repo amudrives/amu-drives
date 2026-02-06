19308142e49c4d169661085e8a11841b1bf58cdf
AMU DRIVES <amudrives@gmail.com>
2026-02-06 19:25:48 +0530

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,14 +1,13 @@
-// Initializes Firebase app using the firebaseConfig exported from firebase-config.js
-// Uses CDN module imports so no npm is required.
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-storage.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
 import { firebaseConfig } from "./firebase-config.js";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const storage = getStorage(app);
+const db = getFirestore(app);
 
-export { app, auth, storage };
+export { app, auth, storage, db };
